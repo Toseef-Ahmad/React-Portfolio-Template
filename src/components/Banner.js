da2a57c8b60a86a85422bb0b78bf8eeb05e0d53f
Toseef-Ahmad <65014307+Toseef-Ahmad@users.noreply.github.com>
2024-07-13 17:36:29 +0500

    Update Content

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -56,8 +56,14 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h1>{`Hi! I'm Touseef Ahmad`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Engineer", "Web Developer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Hello! I'm an enthusiastic software developer with a passion for continuous learning and growth. With four years of experience in React.js development, I have honed my skills in creating dynamic and user-friendly web applications. My journey in tech has been driven by a relentless curiosity and a love for tackling complex problems.
+
+Recently, I've been diving deep into the fascinating world of Artificial Intelligence and exploring the intricate patterns of Data Structures and Algorithms. LeetCode has become my playground where I sharpen my problem-solving abilities and push the boundaries of my logic and creativity.
+
+I believe in the power of persistence and the joy of discovery. Whether it's mastering the latest technology or solving a challenging coding puzzle, I approach every task with enthusiasm and a growth mindset. My goal is to leverage my skills to create impactful solutions and contribute to the ever-evolving tech landscape.
+
+In addition to my technical pursuits, I also enjoy teaching and sharing knowledge. I conduct workshops and participate in community discussions, aiming to inspire and support fellow developers on their learning journeys.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
